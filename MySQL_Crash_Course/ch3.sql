2e558d96e43ba8d7e2db3138090c837ddf3c1569
yanxuanshaozhu <973493269@qq.com>
2020-02-25 01:16:57 +0800

    Upload files

---
+++ MySQL_Crash_Course/ch3.sql
@@ -0,0 +1,21 @@
+use crashcourse;
+
+show databases;
+
+show tables;
+
+show columns from customers;
+desc customers;
+
+show status;
+
+show create database customers;
+
+show create table customers;
+
+show grants;
+
+show errors;
+
+show warnings;
+
